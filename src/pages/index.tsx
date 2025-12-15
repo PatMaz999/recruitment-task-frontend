a84b6049b08ca23c4600d5e3137f0d7e017d1c2b
PatMaz999 <Patryk.m.3456@gmail.com>
2025-12-15 17:41:21 +0100

    update project to TypeScript

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Chart from "../components/Chart";
+
+export default function Home() {
+  return (
+    <div>
+      Hello World. <Link href="/about">About</Link>
+      <Chart />
+    </div>
+  );
+}
